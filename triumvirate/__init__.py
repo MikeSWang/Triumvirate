ae31956da2d25d4fa6c88daa459941c99e9bedc7
Mike S Wang <32841762+MikeSWang@users.noreply.github.com>
2022-02-22 12:02:33 +0000

    Add logging functionalities

---
+++ triumvirate/__init__.py
@@ -0,0 +1,38 @@
+"""
+###########################################################################
+``Triumvirate`` | Measuring 3-Point Correlators in Large-Scale Structure
+###########################################################################
+
+``Triumvirate`` is a Python/C++ package that measures the 3-point
+correlators in large-scale structure clustering analysis.
+
+.. topic:: Licence Statement
+
+    Copyright (C) 2021--2022, M S Wang
+
+    This program is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    This program is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program.  If not, see `<https://www.gnu.org/licenses/>`_.
+
+"""
+__author__ = "Mike S Wang"
+__contact__ = "Mike S Wang"
+__copyright__ = "Copyright 2022, N Sugiyama & M S Wang"
+__date__ = "2022-03-01"
+__description__ = (
+    "Measuring 3-point correlators in "
+    "large-scale structure clustering analysis."
+)
+__email__ = "mikeshengbo.wang@ed.ac.uk"
+__license__ = "GPLv3"
+__url__ = "https://mikeswang.github.io/Triumvirate/"
+__version__ = "0.0.0"
