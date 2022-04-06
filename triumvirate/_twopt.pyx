cff64295bcf765e1ff1aa6957277a17435f9ca68
Mike S Wang <32841762+MikeSWang@users.noreply.github.com>
2022-04-06 16:08:46 +0100

    Wrap all two-pt C++ code

--- triumvirate/_twopt.pyx
+++ triumvirate/_twopt.pyx
@@ -30,20 +30,20 @@ cdef extern from "include/twopt.hpp":
         vector[np.complex128_t] pk_raw
         vector[np.complex128_t] pk_shot
     struct CorrfuncMeasurements:
-        double* rbin
-        double* reff
-        int* npair;
-        np.complex128_t* xi
+        vector[double] rbin
+        vector[double] reff
+        vector[int] npair;
+        vector[np.complex128_t] xi
     struct PowspecWindowMeasurements:
-        double* kbin
-        double* keff
-        int* nmode;
-        np.complex128_t* pk
+        vector[double] kbin
+        vector[double] keff
+        vector[int] nmode;
+        vector[np.complex128_t] pk
     struct CorrfuncWindowMeasurements:
-        double* rbin
-        double* reff
-        int* npair;
-        np.complex128_t* xi
+        vector[double] rbin
+        vector[double] reff
+        vector[int] npair;
+        vector[np.complex128_t] xi
 
     double calc_powspec_normalisation_from_mesh_cpp \
         "calc_powspec_normalisation_from_mesh" (
@@ -66,6 +66,49 @@ cdef extern from "include/twopt.hpp":
         double* kbin,
         double alpha,
         double norm,
+        bool_t save
+    )
+    CorrfuncMeasurements compute_corrfunc_cpp "compute_corrfunc" (
+        CppParticleCatalogue& particles_data,
+        CppParticleCatalogue& particles_rand,
+        LineOfSight* los_data,
+        LineOfSight* los_rand,
+        CppParameterSet& params,
+        double* rbin,
+        double alpha,
+        double norm,
+        bool_t save
+    )
+    # PowspecWindowMeasurements compute_powspec_window_cpp \
+    #     "compute_powspec_window" (
+    #         CppParticleCatalogue& particles_rand,
+    #         LineOfSight* los_rand,
+    #         CppParameterSet& params,
+    #         double* kbin,
+    #         double alpha,
+    #         double norm,
+    #         bool_t save
+    #     )
+    CorrfuncWindowMeasurements compute_corrfunc_window_cpp \
+        "compute_corrfunc_window" (
+            CppParticleCatalogue& particles_rand,
+            LineOfSight* los_rand,
+            CppParameterSet& params,
+            double* rbin,
+            double alpha,
+            double norm,
+            bool_t save
+        )
+    PowspecMeasurements compute_powspec_in_box_cpp "compute_powspec_in_box" (
+        CppParticleCatalogue& particles_data,
+        CppParameterSet& params,
+        double* kbin,
+        bool_t save
+    )
+    CorrfuncMeasurements compute_corrfunc_in_box_cpp "compute_corrfunc_in_box" (
+        CppParticleCatalogue& particles_data,
+        CppParameterSet& params,
+        double* rbin,
         bool_t save
     )
 
@@ -132,3 +175,165 @@ def _compute_powspec(
         'pk_shot': np.asarray(meas.pk_shot),
     }
 
+def _compute_corrfunc(
+        _ParticleCatalogue particles_data not None,
+        _ParticleCatalogue particles_rand not None,
+        np.ndarray[double, ndim=2, mode='c'] los_data not None,
+        np.ndarray[double, ndim=2, mode='c'] los_rand not None,
+        ParameterSet params not None,
+        np.ndarray[double, ndim=1, mode='c'] rbin not None,
+        double alpha,
+        double norm,
+        bool_t save
+    ):
+
+    # Parse lines of sight per particle.
+    cdef LineOfSight* los_data_cpp = <LineOfSight*>malloc(
+        len(los_data) * sizeof(LineOfSight)
+    )
+    for pid, (los_x, los_y, los_z) in enumerate(los_data):
+        los_data_cpp[pid].pos[0] = los_x
+        los_data_cpp[pid].pos[1] = los_y
+        los_data_cpp[pid].pos[2] = los_z
+
+    cdef LineOfSight* los_rand_cpp = <LineOfSight*>malloc(
+        len(los_rand) * sizeof(LineOfSight)
+    )
+    for pid, (los_x, los_y, los_z) in enumerate(los_rand):
+        los_rand_cpp[pid].pos[0] = los_x
+        los_rand_cpp[pid].pos[1] = los_y
+        los_rand_cpp[pid].pos[2] = los_z
+
+    # Run algorithm.
+    cdef CorrfuncMeasurements meas
+    meas = compute_corrfunc_cpp(
+        deref(particles_data.thisptr), deref(particles_rand.thisptr),
+        los_data_cpp, los_rand_cpp,
+        deref(params.thisptr),
+        &rbin[0], alpha, norm,
+        save
+    )
+
+    return {
+        'rbin': np.asarray(meas.rbin),
+        'reff': np.asarray(meas.reff),
+        'npair': np.asarray(meas.npair),
+        'xi': np.asarray(meas.xi),
+    }
+
+# def _compute_powspec_window(
+#         _ParticleCatalogue particles_rand not None,
+#         np.ndarray[double, ndim=2, mode='c'] los_rand not None,
+#         ParameterSet params not None,
+#         np.ndarray[double, ndim=1, mode='c'] kbin not None,
+#         double alpha,
+#         double norm,
+#         bool_t save
+#     ):
+#
+#     # Parse lines of sight per particle.
+#     cdef LineOfSight* los_rand_cpp = <LineOfSight*>malloc(
+#         len(los_rand) * sizeof(LineOfSight)
+#     )
+#     for pid, (los_x, los_y, los_z) in enumerate(los_rand):
+#         los_rand_cpp[pid].pos[0] = los_x
+#         los_rand_cpp[pid].pos[1] = los_y
+#         los_rand_cpp[pid].pos[2] = los_z
+#
+#     # Run algorithm.
+#     cdef PowspecWindowMeasurements meas
+#     meas = compute_powspec_window_cpp(
+#         deref(particles_rand.thisptr),
+#         los_rand_cpp,
+#         deref(params.thisptr),
+#         &kbin[0], alpha, norm,
+#         save
+#     )
+#
+#     return {
+#         'kbin': np.asarray(meas.kbin),
+#         'keff': np.asarray(meas.keff),
+#         'nmode': np.asarray(meas.nmode),
+#         'pk': np.asarray(meas.pk_raw),
+#     }
+
+def _compute_corrfunc_window(
+        _ParticleCatalogue particles_rand not None,
+        np.ndarray[double, ndim=2, mode='c'] los_rand not None,
+        ParameterSet params not None,
+        np.ndarray[double, ndim=1, mode='c'] rbin not None,
+        double alpha,
+        double norm,
+        bool_t save
+    ):
+
+    # Parse lines of sight per particle.
+    cdef LineOfSight* los_rand_cpp = <LineOfSight*>malloc(
+        len(los_rand) * sizeof(LineOfSight)
+    )
+    for pid, (los_x, los_y, los_z) in enumerate(los_rand):
+        los_rand_cpp[pid].pos[0] = los_x
+        los_rand_cpp[pid].pos[1] = los_y
+        los_rand_cpp[pid].pos[2] = los_z
+
+    # Run algorithm.
+    cdef CorrfuncWindowMeasurements meas
+    meas = compute_corrfunc_window_cpp(
+        deref(particles_rand.thisptr),
+        los_rand_cpp,
+        deref(params.thisptr),
+        &rbin[0], alpha, norm,
+        save
+    )
+
+    return {
+        'rbin': np.asarray(meas.rbin),
+        'reff': np.asarray(meas.reff),
+        'npair': np.asarray(meas.npair),
+        'xi': np.asarray(meas.xi),
+    }
+
+def _compute_powspec_in_box(
+        _ParticleCatalogue particles_data not None,
+        ParameterSet params not None,
+        np.ndarray[double, ndim=1, mode='c'] kbin not None,
+        bool_t save
+    ):
+
+    cdef PowspecMeasurements meas
+    meas = compute_powspec_in_box_cpp(
+        deref(particles_data.thisptr),
+        deref(params.thisptr),
+        &kbin[0],
+        save
+    )
+
+    return {
+        'kbin': np.asarray(meas.kbin),
+        'keff': np.asarray(meas.keff),
+        'nmode': np.asarray(meas.nmode),
+        'pk_raw': np.asarray(meas.pk_raw),
+        'pk_shot': np.asarray(meas.pk_shot),
+    }
+
+def _compute_corrfunc_in_box(
+        _ParticleCatalogue particles_data not None,
+        ParameterSet params not None,
+        np.ndarray[double, ndim=1, mode='c'] rbin not None,
+        bool_t save
+    ):
+
+    cdef CorrfuncMeasurements meas
+    meas = compute_corrfunc_in_box_cpp(
+        deref(particles_data.thisptr),
+        deref(params.thisptr),
+        &rbin[0],
+        save
+    )
+
+    return {
+        'rbin': np.asarray(meas.rbin),
+        'reff': np.asarray(meas.reff),
+        'npair': np.asarray(meas.npair),
+        'xi': np.asarray(meas.xi),
+    }
