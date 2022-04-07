e0a4840cd9f52fd350b4b35ced1ab33cf4bfa479
MikeSWang <wsb3513@gmail.com>
2022-04-07 14:52:16 +0100

    Fix box alignment and LoS conflict

--- triumvirate/twopt.py
+++ triumvirate/twopt.py
@@ -32,7 +32,9 @@ def _prepare_catalogue(catalogue):
     )
 
 def compute_powspec(catalogue_data, catalogue_rand, params,
-                    los_data=None, los_rand=None, save=False, logger=None):
+                    los_data=None, los_rand=None,
+                    box_align='centre', ngrid_pad=None,
+                    save=False, logger=None):
     """Compute power spectrum from data and random catalogues.
 
     Parameters
@@ -51,6 +53,15 @@ def compute_powspec(catalogue_data, catalogue_rand, params,
         Specified lines of sight for the random-source catalogue.
         If `None` (default), this is automatically computed using
         :meth:`~triumvirate.catalogue.ParticleCatalogue.compute_los`.
+    box_align : {'centre', 'pad'}, optional
+        Alignment of the catalogue(s) inside the box.  The random-source
+        catalogue is used as the reference to 'centre' (default) or
+        'pad' (away from the box origin corner) both catalogues.
+    ngrid_pad : (array of) float, optional
+        Number of grids as padding for the catalogues away from the
+        origin corner of the box.  If `None` (default), the default
+        padding factor is assumed (see
+        :meth:`triumvirate.catalogue.ParticleCatalogue.pad_catalogues`).
     save : bool, optional
         If `True` (default is `False`), measurement results are
         automatically saved to an output file specified from `params`.
@@ -64,15 +75,6 @@ def compute_powspec(catalogue_data, catalogue_rand, params,
 
     """
     # Prepare catalogues.
-    catalogue_data.centre_catalogues(
-        [params['boxsize'][axis] for axis in ['x', 'y', 'z']],
-        catalogue_ref=catalogue_rand
-    )
-
-    particles_data = _prepare_catalogue(catalogue_data)
-    particles_rand = _prepare_catalogue(catalogue_rand)
-
-    # Compute auxiliary quantities.
     if los_data is None:
         los_data = catalogue_data.compute_los()
     if los_rand is None:
@@ -81,6 +83,26 @@ def compute_powspec(catalogue_data, catalogue_rand, params,
     los_data = np.ascontiguousarray(los_data)
     los_rand = np.ascontiguousarray(los_rand)
 
+    if box_align.lower() == 'centre':
+        catalogue_data.centre_catalogues(
+            [params['boxsize'][axis] for axis in ['x', 'y', 'z']],
+            catalogue_ref=catalogue_rand
+        )
+    elif box_align.lower() == 'pad':
+        kwargs = {} if ngrid_pad is None else {'ngrid_pad': ngrid_pad}
+        catalogue_data.pad_catalogues(
+            [params['boxsize'][axis] for axis in ['x', 'y', 'z']],
+            [params['ngrid'][axis] for axis in ['x', 'y', 'z']],
+            catalogue_ref=catalogue_rand,
+            **kwargs
+        )
+    else:
+        raise ValueError("`box_alignment` must be 'centre' or 'pad'.")
+
+    particles_data = _prepare_catalogue(catalogue_data)
+    particles_rand = _prepare_catalogue(catalogue_rand)
+
+    # Compute auxiliary quantities.
     kbin = np.ascontiguousarray(
         np.linspace(*params['range'], num=params['dim'])
     )
@@ -129,7 +151,9 @@ def compute_powspec(catalogue_data, catalogue_rand, params,
     return results
 
 def compute_corrfunc(catalogue_data, catalogue_rand, params,
-                     los_data=None, los_rand=None, save=False, logger=None):
+                     los_data=None, los_rand=None,
+                     box_align='centre', ngrid_pad=None,
+                     save=False, logger=None):
     """Compute correlation function from data and random catalogues.
 
     Parameters
@@ -148,6 +172,15 @@ def compute_corrfunc(catalogue_data, catalogue_rand, params,
         Specified lines of sight for the random-source catalogue.
         If `None` (default), this is automatically computed using
         :meth:`~triumvirate.catalogue.ParticleCatalogue.compute_los`.
+    box_align : {'centre', 'pad'}, optional
+        Alignment of the catalogue(s) inside the box.  The random-source
+        catalogue is used as the reference to 'centre' (default) or
+        'pad' (away from the box origin corner) both catalogues.
+    ngrid_pad : (array of) float, optional
+        Number of grids as padding for the catalogues away from the
+        origin corner of the box.  If `None` (default), the default
+        padding factor is assumed (see
+        :meth:`triumvirate.catalogue.ParticleCatalogue.pad_catalogues`).
     save : bool, optional
         If `True` (default is `False`), measurement results are
         automatically saved to an output file specified from `params`.
@@ -161,15 +194,6 @@ def compute_corrfunc(catalogue_data, catalogue_rand, params,
 
     """
     # Prepare catalogues.
-    catalogue_data.centre_catalogues(
-        [params['boxsize'][axis] for axis in ['x', 'y', 'z']],
-        catalogue_ref=catalogue_rand
-    )
-
-    particles_data = _prepare_catalogue(catalogue_data)
-    particles_rand = _prepare_catalogue(catalogue_rand)
-
-    # Compute auxiliary quantities.
     if los_data is None:
         los_data = catalogue_data.compute_los()
     if los_rand is None:
@@ -178,6 +202,26 @@ def compute_corrfunc(catalogue_data, catalogue_rand, params,
     los_data = np.ascontiguousarray(los_data)
     los_rand = np.ascontiguousarray(los_rand)
 
+    if box_align.lower() == 'centre':
+        catalogue_data.centre_catalogues(
+            [params['boxsize'][axis] for axis in ['x', 'y', 'z']],
+            catalogue_ref=catalogue_rand
+        )
+    elif box_align.lower() == 'pad':
+        kwargs = {} if ngrid_pad is None else {'ngrid_pad': ngrid_pad}
+        catalogue_data.pad_catalogues(
+            [params['boxsize'][axis] for axis in ['x', 'y', 'z']],
+            [params['ngrid'][axis] for axis in ['x', 'y', 'z']],
+            catalogue_ref=catalogue_rand,
+            **kwargs
+        )
+    else:
+        raise ValueError("`box_alignment` must be 'centre' or 'pad'.")
+
+    particles_data = _prepare_catalogue(catalogue_data)
+    particles_rand = _prepare_catalogue(catalogue_rand)
+
+    # Compute auxiliary quantities.
     rbin = np.ascontiguousarray(
         np.linspace(*params['range'], num=params['dim'])
     )
@@ -252,6 +296,10 @@ def compute_corrfunc(catalogue_data, catalogue_rand, params,
 #
 #     """
 #     # Prepare catalogues.
+#     if los_rand is None:
+#         los_rand = catalogue_rand.compute_los()
+#     los_rand = np.ascontiguousarray(los_rand)
+#
 #     catalogue_rand.centre(
 #         [params['boxsize'][axis] for axis in ['x', 'y', 'z']]
 #     )
@@ -259,10 +307,6 @@ def compute_corrfunc(catalogue_data, catalogue_rand, params,
 #     particles_rand = _prepare_catalogue(catalogue_rand)
 #
 #     # Compute auxiliary quantities.
-#     if los_rand is None:
-#         los_rand = catalogue_rand.compute_los()
-#     los_rand = np.ascontiguousarray(los_rand)
-#
 #     kbin = np.ascontiguousarray(
 #         np.linspace(*params['range'], num=params['dim'])
 #     )
@@ -335,6 +379,10 @@ def compute_corrfunc_window(catalogue_rand, params, los_rand=None,
 
     """
     # Prepare catalogues.
+    if los_rand is None:
+        los_rand = catalogue_rand.compute_los()
+    los_rand = np.ascontiguousarray(los_rand)
+
     catalogue_rand.centre(
         [params['boxsize'][axis] for axis in ['x', 'y', 'z']]
     )
@@ -342,10 +390,6 @@ def compute_corrfunc_window(catalogue_rand, params, los_rand=None,
     particles_rand = _prepare_catalogue(catalogue_rand)
 
     # Compute auxiliary quantities.
-    if los_rand is None:
-        los_rand = catalogue_rand.compute_los()
-    los_rand = np.ascontiguousarray(los_rand)
-
     rbin = np.ascontiguousarray(
         np.linspace(*params['range'], num=params['dim'])
     )
