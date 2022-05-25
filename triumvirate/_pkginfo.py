6a5656b1d2b3fbe3cda42ca0123f5973ecff7cb3
MikeSWang <wsb3513@gmail.com>
2022-05-25 12:17:44 +0100

    Make PEP-8 and pip compliant

---
+++ triumvirate/_pkginfo.py
@@ -0,0 +1,18 @@
+"""Store package information as global variables.
+
+Only string variables can be defined in this module.  No class or function
+definition, implementation or execution is allowed.
+
+"""
+__author__ = "Mike S Wang, N Sugiyama"
+__contact__ = "Mike S Wang"
+__copyright__ = "Copyright 2022, N Sugiyama & M S Wang"
+__date__ = "2022-03-01"
+__description__ = (
+    "Three-point clustering correlators in large-scale structure analysis."
+)
+__email__ = "mikeshengbo.wang@ed.ac.uk"
+__license__ = "GPLv3"
+__version__ = "0.0.0"
+
+_release_ = "development"
