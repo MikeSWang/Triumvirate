8420ea3861052a96c22b473b857c37206fc92ffa
Mike S Wang <32841762+MikeSWang@users.noreply.github.com>
2022-08-24 14:08:30 +0100

    Change nmesh to unsigned int

--- triumvirate/include/parameters.hpp
+++ triumvirate/include/parameters.hpp
@@ -78,7 +78,7 @@ class ParameterSet {
 
   /// Derived quantities.
   double volume;  ///< box volume
-  int nmesh;  ///< total mesh grid number
+  unsigned int nmesh;  ///< total mesh grid number
 
   /// Misc.
   int batch_number;  ///< job batch number
