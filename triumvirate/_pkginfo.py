cb94190d3ae491bf01abb2c07695e5933f767d4e
Mike S Wang <32841762+MikeSWang@users.noreply.github.com>
2022-10-21 16:42:59 +0100

    Update package info

--- triumvirate/_pkginfo.py
+++ triumvirate/_pkginfo.py
@@ -1,18 +1,17 @@
-"""Store package information as global variables.
+"""Store package information as global special variables.
 
 Only string variables can be defined in this module.  No class or function
-definition, implementation or execution is allowed.
+definition or execution is allowed.
 
 """
-__author__ = "Mike S Wang, N Sugiyama"
+__author__ = "Mike S Wang, Naonori Sugiyama"
 __contact__ = "Mike S Wang"
-__copyright__ = "Copyright 2022, N Sugiyama & M S Wang"
+__copyright__ = "Copyright 2022, M S Wang & N Sugiyama"
 __date__ = "2022-03-01"
 __description__ = (
-    "Three-point clustering correlators in large-scale structure analysis."
+    "Two- and three-point clustering statistics "
+    "in large-scale structure analysis."
 )
 __email__ = "mikeshengbo.wang@ed.ac.uk"
 __license__ = "GPLv3"
-__version__ = "0.0.0"
-
-_release_ = "development"
+__version__ = "0.0a0"
