bad814b2815b23e4e82937746ba7e50bd9612717
MikeSWang <wsb3513@gmail.com>
2022-11-17 00:51:29 +0000

    Update window convolution

--- triumvirate/winconv.py
+++ triumvirate/winconv.py
@@ -8,7 +8,7 @@ Perform window convolution of three-point statistics.
 from typing import NamedTuple
 
 import numpy as np
-from scipy.interpolate import RectBivariateSpline
+from scipy.interpolate import interp1d, RectBivariateSpline
 
 from triumvirate.bihankel import (
     transform_bispec_to_3pcf,
@@ -52,8 +52,8 @@ class _WConvTerm(NamedTuple):
 
     For example, `term = _WConvTerm('000', 'ic', -1)`, with
     parameters/attributes/factors ``term.deg_Q = '000'``,
-    ``term.deg_zeta = 'ic'` and ``term.coeff = -1`` corresponds to the
-    term :math:`- Q_{000} \zeta_{\mathrm{ic}}`.
+    ``term.deg_zeta = 'ic'` and ``term.coeff = -1``, corresponds to the
+    term :math:`- 1 \times Q_{000} \zeta_{\mathrm{ic}}`.
 
     Attributes
     ----------
@@ -112,35 +112,35 @@ class _WConvFormulae:
             for term in formula
         ])
 
-    def __getitem__(self, deg):
+    def __getitem__(self, multipole):
         """Get the formula for the window convolution of a specific
         3PCF multipole.
 
         Parameters
         ----------
-        deg : str
-            Degree of the specified windowed 3PCF multipole.
+        multipole : str
+            Degrees of the specified windowed 3PCF multipole.
 
         Returns
         -------
-        _type_
-            _description_
+        list of :class:`~triumvirate.winconv._WConvTerm`
+            List of window convolution terms for `multipole`.
+
         """
-        return self._formulae[deg]
+        return self._formulae[multipole]
 
 
-def wconv_3pcf(formulae, zeta, Q, r1_com, r2_com,
-               r1_zeta=None, r2_zeta=None, r1_Q=None, r2_Q=None):
+def wconv_3pcf_diag(formulae, Q, zeta, r_common, r_Q=None, r_zeta=None):
     """Convolve multipoles of the 3-point correlation function (3PCF) and
-    the window function using specified formulae.
+    the window function in diagonal form using specified formulae.
 
     Parameters
     ----------
     formulae : dict of {str: sequence of tuples of (str, str, float)} or 'sugiyama+19'
         Window convolution formulae encoded as a dictionary where for
-        each convolved-multipole degree key, the value is a sequence of
-        tuples each corresponding to a window convolution term's
-        multipole factors and the numerical coefficient.
+        each convolved-multipole degree (the key), the value is
+        a sequence of tuples each corresponding to a window convolution
+        term including the numerical coefficient.
 
         Example:
 
@@ -154,34 +154,134 @@ def wconv_3pcf(formulae, zeta, Q, r1_com, r2_com,
                 ],
             }
 
-        computes only the multipole
+        computes only the '000' monopole
 
         .. math::
 
             \tilde{\zeta}_{000} =
-                Q_{000} (\zeta_{000} - \zeta_{\mathrm{ic}})
+                1 \times Q_{000} \zeta_{000}
+                + (-1) \times Q_{000} \zeta_{\mathrm{ic}}
                 + \frac{1}{3} Q_{110} \zeta_{110} \,.
 
-        If a string (namely 'sugiyama19') is passed, the window
-        convolution formula from Sugiyama et al. (2019)
-        [`arXiv:1803.02132 <Sugiyama+19>`_].
+        If a named formula (e.g. 'sugiyama19') is passed, the window
+        convolution formula follows that prescription (e.g. Sugiyama
+        et al. 2019 [`arXiv:1803.02132 <Sugiyama+19>`_]).
 
-    zeta : dict of {str: 2-d array of float}
-        Unwindowed 3PCF multipole samples.
     Q : dict of {str: 2-d array of float}
-        Window function multipole samples.
-    r1_com, r2_com : 1-d array of float
-        Common separation sample points for both `zeta` and `Q`.
-        If `r1_zeta` and `r1_Q` (or `r2_zeta` and `r2_Q`) are provided,
-        all multipole samples are interpolated over `r1_com` and `r2_com`;
-        otherwise all multipole samples are assumed to correspond to
-        `r1_com` and `r2_com`.
-    r1_zeta, r2_zeta : 1-d array of float, optional
+        Window function multipole samples (each key is a multipole).
+    zeta : dict of {str: 2-d array of float}
+        Unwindowed 3PCF multipole samples (each key is a multipole).
+    r_common : 1-d array of float
+        Common separation sample points for both `Q` and `zeta`.
+        If `r_Q` or `r_zeta` is provided, all multipole samples are
+        re-interpolated over `r_common`; otherwise all multipole
+        sample points are assumed to be `r_common`.
+    r_Q : 1-d array of float, optional
+        Separation sample points for `Q`.  If `None` (default),
+        `r_common` is assumed instead.
+    r_zeta : 1-d array of float, optional
         Separation sample points for `zeta`.  If `None` (default),
-        `r1_com` and `r2_com` are used instead.
+        `r_common` is assumed instead.
+
+    Returns
+    -------
+    zeta_wconv : dict of {str: :class:`numpy.ndarray`}
+        Window-convolved 3PCF samples.
+
+    """
+    # Fetch named formulae as a dictionary.
+    if isinstance(formulae, str) and formulae.lower() == 'sugiyama+19':
+        formulae = _FORMULAE_SUGIYAMA19
+
+    # Convert dictionary to formular object.
+    if not isinstance(formulae, _WConvFormulae):
+        formulae = _WConvFormulae(formulae)
+
+    # Coerce onto the same coordinate samples.
+    if r_Q is None:
+        Q_samples = Q
+    else:
+        r_Q = r_Q if r_Q is not None else r_common
+        Q_samples = {
+            multipole: interp1d(r_Q, Q[multipole])(r_common)
+            for multipole in Q.keys()
+        }
+
+    if r_zeta is None:
+        zeta_samples = zeta
+    else:
+        r_zeta = r_zeta if r_zeta is not None else r_common
+        zeta_samples = {
+            multipole: interp1d(r_zeta, zeta[multipole])(r_common)
+            for multipole in zeta.keys()
+        }
+
+    # Perform convolution as multiplication.
+    zeta_conv = {}
+    for multipole in formulae.multipoles:
+        zeta_tilde = 0.  # technically not a scalar
+        for term in formulae[multipole]:
+            zeta_tilde += term.coeff \
+                * Q_samples[term.deg_Q] * zeta_samples[term.deg_zeta]
+        zeta_conv[multipole] = zeta_tilde
+
+    return zeta_conv
+
+
+def wconv_3pcf(formulae, Q, zeta, r1_common, r2_common,
+               r1_Q=None, r2_Q=None, r1_zeta=None, r2_zeta=None):
+    """Convolve multipoles of the 3-point correlation function (3PCF) and
+    the window function using specified formulae.
+
+    Parameters
+    ----------
+    formulae : dict of {str: sequence of tuples of (str, str, float)} or 'sugiyama+19'
+        Window convolution formulae encoded as a dictionary where for
+        each convolved-multipole degree (the key), the value is
+        a sequence of tuples each corresponding to a window convolution
+        term including the numerical coefficient.
+
+        Example:
+
+        .. code-block::
+
+            formulae = {
+                '000': [
+                    ('000', '000', 1),
+                    ('000', 'ic', -1),
+                    ('110', '110', 1./3),
+                ],
+            }
+
+        computes only the '000' monopole
+
+        .. math::
+
+            \tilde{\zeta}_{000} =
+                1 \times Q_{000} \zeta_{000}
+                + (-1) \times Q_{000} \zeta_{\mathrm{ic}}
+                + \frac{1}{3} Q_{110} \zeta_{110} \,.
+
+        If a named formula (e.g. 'sugiyama19') is passed, the window
+        convolution formula follows that prescription (e.g. Sugiyama
+        et al. 2019 [`arXiv:1803.02132 <Sugiyama+19>`_]).
+
+    Q : dict of {str: 2-d array of float}
+        Window function multipole samples (each key is a multipole).
+    zeta : dict of {str: 2-d array of float}
+        Unwindowed 3PCF multipole samples (each key is a multipole).
+    r1_common, r2_common : 1-d array of float
+        Common separation sample points for both `Q` and `zeta`.
+        If `r1_Q`/`r1_zeta` or `r2_Q`/`r2_zeta` is provided,
+        all multipole samples are re-interpolated over `r1_common`
+        and `r2_common`; otherwise all multipole sample points are assumed
+        to be `r1_common` and `r2_common`.
     r1_Q, r2_Q : 1-d array of float, optional
         Separation sample points for `Q`.  If `None` (default),
-        `r1_com` and `r2_com` are used instead.
+        `r1_common` and `r2_common` are assumed instead.
+    r1_zeta, r2_zeta : 1-d array of float, optional
+        Separation sample points for `zeta`.  If `None` (default),
+        `r1_common` and `r2_common` are assumed instead.
 
     Returns
     -------
@@ -191,36 +291,43 @@ def wconv_3pcf(formulae, zeta, Q, r1_com, r2_com,
     .. _Sugiyama+19: https://arxiv.org/abs/1803.02132
 
     """
+    # Fetch named formulae as a dictionary.
     if isinstance(formulae, str) and formulae.lower() == 'sugiyama+19':
         formulae = _FORMULAE_SUGIYAMA19
+
+    # Convert dictionary to formular object.
     if not isinstance(formulae, _WConvFormulae):
         formulae = _WConvFormulae(formulae)
+
+    # Coerce onto the same coordinate samples.
+    if r1_Q is None and r2_Q is None:
+        Q_samples = Q
+    else:
+        r1_Q = r1_Q if r1_Q is not None else r1_common
+        r2_Q = r2_Q if r2_Q is not None else r2_common
+        Q_samples = {
+            multipole: RectBivariateSpline(
+                r1_Q, r2_Q, Q[multipole]
+            )(r1_common, r2_common)
+            for multipole in Q.keys()
+        }
 
     if r1_zeta is None and r2_zeta is None:
         zeta_samples = zeta
     else:
-        r1_zeta = r1_zeta if r1_zeta is not None else r1_com
-        r2_zeta = r2_zeta if r2_zeta is not None else r2_com
+        r1_zeta = r1_zeta if r1_zeta is not None else r1_common
+        r2_zeta = r2_zeta if r2_zeta is not None else r2_common
         zeta_samples = {
-            deg: RectBivariateSpline(
-                r1_zeta, r2_zeta, zeta[deg]
-            )(r1_com, r2_com)
-            for deg in zeta.keys()
+            multipole: RectBivariateSpline(
+                r1_zeta, r2_zeta, zeta[multipole]
+            )(r1_common, r2_common)
+            for multipole in zeta.keys()
         }
 
-    if r1_Q is None and r2_Q is None:
-        Q_samples = Q
-    else:
-        r1_Q = r1_Q if r1_Q is not None else r1_com
-        r2_Q = r2_Q if r2_Q is not None else r2_com
-        Q_samples = {
-            deg: RectBivariateSpline(r1_Q, r2_Q, Q[deg])(r1_com, r2_com)
-            for deg in Q.keys()
-        }
-
+    # Perform convolution as multiplication.
     zeta_conv = {}
     for multipole in formulae.multipoles:
-        zeta_tilde = 0.
+        zeta_tilde = 0.  # technically not a scalar
         for term in formulae[multipole]:
             zeta_tilde += term.coeff \
                 * Q_samples[term.deg_Q] * zeta_samples[term.deg_zeta]
@@ -229,38 +336,44 @@ def wconv_3pcf(formulae, zeta, Q, r1_com, r2_com,
     return zeta_conv
 
 
-def wconv_bispec(formulae, bk, k_in, k_out, Q, r_com, r1_Q=None, r2_Q=None,
+def wconv_bispec(formulae, Q, bk, k, k_out, r_common, r_Q=None,
                  transform_kwargs=None):
     """Convolve multipoles of the bispectrum and the window function
     via configuration space using specified formulae.
+
+    Notes
+    -----
+    This function assumes the wavenumber samples :math:`k_{1,2}` in the
+    bispectrum :math:`B_{\ell_1 \ell_2 L}(k_1, k_2)` are the same.
 
     Parameters
     ----------
     formulae : dict of {str: sequence of tuples of (str, str, float)} or 'sugiyama+19'
         See :func:`~winconv.wconv.wconv_3pcf`.
-    bk : dict of {str: 2-d array of float}
-        Unwindowed input bispectrum multipoles.
-    k_in : 1-d array of float
-        Input wavenumbers for which `bk` is evaluated.
-    k_out : 1-d array of float
-        Output wavenumbers for which the window-convolved bispectrum
-        is evaluated.
     Q : dict of {str: 2-d array of float}
-        Window function multipoles.
-    r_com : 1-d array of float
-        Common separation sample points for both :math:`zeta` (transformed
-        from `bk`) and `Q`.  If `r1_Q` (or `r2_Q`) is provided, all
-        multipole samples are interpolated over `r_com`; otherwise all
-        multipole samples are assumed to correspond to `r_com`.
-    r1_Q, r2_Q : 1-d array of float, optional
-        See :func:`~winconv.wconv.wconv_3pcf`.
+        Configuration-space window function multipole samples
+        (each key is a multipole).
+    bk : dict of {str: 2-d array of float}
+        Unwindowed bispectrum multipole samples (each key is a multipole).
+    k : 1-d array of float
+        Unwindowed bispectrum wavenumber samples.
+    k_out : 1-d array of float
+        Output bispectrum wavenumber samples.
+    r_common : 1-d array of float
+        Common separation sample points for both `Q` and 3PCF :math:`zeta`
+        (transformed from `bk`).  If `r_Q` is provided, all multipole
+        samples are re-interpolated over `r_common`; otherwise all
+        multipole sample points are assumed to be `r_common`.
+    r_Q : 1-d array of float, optional
+        Separation sample points for `Q`.  If `None` (default),
+        `r_common` is assumed instead.
     transform_kwargs : dict, optional
         Keyword arguments to be passed to
         :class:`triumvirate.bihankel.transform_bispec_to_3pcf` and
         :class:`triumvirate.bihankel.transform_3pcf_to_bispec` for
         transforming to/from configuration space.  If `None` (default),
-        the sample number for FFTLog transform is set to 1024 and no
-        extrapolation is performed.
+        the FFTLog sample number is set to 1024 and no extrapolation
+        is performed.
 
     Returns
     -------
@@ -273,19 +386,22 @@ def wconv_bispec(formulae, bk, k_in, k_out, Q, r_com, r1_Q=None, r2_Q=None,
     n_fftlog = transform_kwargs.get('n_fftlog', 1024)
 
     zeta = {}
-    for deg, bk_in in bk.items():
-        zeta[deg] = transform_bispec_to_3pcf(
-            int(deg[0]), int(deg[1]), bk_in, k_in, r_com, n_fftlog, **transform_kwargs
+    for multipole, bk_in in bk.items():
+        ell1, ell2 = int(multipole[0]), int(multipole[1])
+        zeta[multipole] = transform_bispec_to_3pcf(
+            ell1, ell2, k, bk_in, r_common, n_fftlog,
+            **transform_kwargs
         )['zeta']
 
     zeta_wconv = wconv_3pcf(
-        formulae, zeta, Q, r_com, r_com, r1_Q=r1_Q, r2_Q=r2_Q
+        formulae, Q, zeta, r_common, r_common, r1_Q=r_Q, r2_Q=r_Q
     )
 
     bk_wconv = {}
-    for deg, zeta_in in zeta_wconv.items():
-        bk_wconv[deg] = transform_3pcf_to_bispec(
-            int(deg[0]), int(deg[1]), zeta_in, r_com, k_out, n_fftlog
+    for multipole, zeta_in in zeta_wconv.items():
+        ell1, ell2 = int(multipole[0]), int(multipole[1])
+        bk_wconv[multipole] = transform_3pcf_to_bispec(
+            ell1, ell2, r_common, zeta_in, k_out, n_fftlog
         )
 
     return bk_wconv
