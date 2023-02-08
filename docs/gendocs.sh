2965532188f6b435e853afe415df6bda707074bd
MikeSWang <wsb3513@gmail.com>
2023-02-08 16:04:49 +0000

    Update doc generation

--- docs/gendocs.sh
+++ docs/gendocs.sh
@@ -6,7 +6,7 @@ cd ${DOCS_DIR}
 if [ ! -d api ]; then mkdir -p ./source/api; fi
 
 make clean
-sphinx-apidoc -efEMT -d 1 -t ./source/_templates/ -o ./source/api ../triumvirate ../**/tests/*
-rm ./source/api/triumvirate.rst
+sphinx-apidoc -efEMT -d 1 -t ./source/_templates/ -o ./source/apidoc ../triumvirate ../**/tests/*
+rm ./source/apidoc/triumvirate.rst
 make html
 cd ..
