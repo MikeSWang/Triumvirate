ddb326a96b8ff7467cf9ca8bf13d9d9d42aa85e5
MikeSWang <wsb3513@gmail.com>
2023-02-13 23:47:21 +0000

    Configure doxygen. Update docstrings

--- docs/gendocs.sh
+++ docs/gendocs.sh
@@ -6,6 +6,8 @@ DOCS_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 cd ${DOCS_DIR}
 
 # Set API doc directories and paths.
+DOXY_CONF_FILE=source/doxygen.conf
+
 APIDOC_PY_DIR=./source/apidoc_py
 TMPL_DIR=./source/_templates/
 EXCL_DIRS="../triumvirate ../**/tests/*"
@@ -15,6 +17,7 @@ RM_FILES="${APIDOC_PY_DIR}/triumvirate.rst"
 # Build docs.
 make clean
 
+doxygen ${DOXY_CONF_FILE}
 sphinx-apidoc -efEMT -d 1 -t ${TMPL_DIR} -o ${APIDOC_PY_DIR} ${EXCL_DIRS}
 rm ${RM_FILES}
 
