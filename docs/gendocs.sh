95718390e0a23a0ee2a223a48c855e397a5125b7
Mike S Wang <32841762+MikeSWang@users.noreply.github.com>
2023-02-15 14:41:48 +0000

    Automate doxygen build

--- docs/gendocs.sh
+++ docs/gendocs.sh
@@ -7,6 +7,7 @@ cd ${DOCS_DIR}
 
 # Set API doc directories and paths.
 DOXY_CONF_FILE=source/Doxyfile.conf
+DOXY_BUILD_DIR="./build\/doxygen\/"
 
 APIDOC_PY_DIR=./source/apidoc_py
 TMPL_DIR=./source/_templates/
@@ -17,7 +18,16 @@ RM_FILES="${APIDOC_PY_DIR}/triumvirate.rst"
 # Build docs.
 make clean
 
-doxygen ${DOXY_CONF_FILE}
+cp ${DOXY_CONF_FILE} ${DOXY_CONF_FILE}.temp
+sed -i "s/OUTPUT_DIRECTORY .*=.*/OUTPUT_DIRECTORY       = ${DOXY_BUILD_DIR}/g"\
+    ${DOXY_CONF_FILE}.temp
+sed -i "s/GENERATE_HTML .*=.*/GENERATE_HTML          = YES/g"\
+    ${DOXY_CONF_FILE}.temp
+sed -i "s/GENERATE_XML .*=.*/GENERATE_XML           = NO/g"\
+    ${DOXY_CONF_FILE}.temp
+doxygen ${DOXY_CONF_FILE}.temp
+rm ${DOXY_CONF_FILE}.temp
+
 sphinx-apidoc -efEMT -d 1 -t ${TMPL_DIR} -o ${APIDOC_PY_DIR} ${EXCL_DIRS}
 rm ${RM_FILES}
 
