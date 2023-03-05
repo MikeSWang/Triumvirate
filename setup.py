7cd22512c06818ae36b2892cbdb7bcc15609fce9
Mike S Wang <32841762+MikeSWang@users.noreply.github.com>
2023-03-05 23:34:03 +0000

    Enhance build processes

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@
 import os
 import platform
 from setuptools import setup
+from setuptools.command.build_clib import build_clib
 
 from Cython.Build import cythonize
 from Cython.Distutils import build_ext, Extension
@@ -53,11 +54,13 @@ os.environ['CXX'] = os.environ.get('PY_CXX', compiler)
 # Modify compilation options.
 cflags = os.environ.get('PY_CFLAGS', '').split()
 ldflags = [
-    lib for lib in os.environ.get('PY_LDFLAGS', '').split()
+    lib
+    for lib in os.environ.get('PY_LDFLAGS', '').split()
     if not lib.startswith('-l')
 ]
 
-py_trvomp = os.environ.get('PY_TRVOMP')  # reserved env. var. for ``setup.py``
+# Add optional compilation option: OpenMP.
+py_trvomp = os.environ.get('PY_TRVOMP')
 if py_trvomp is not None and py_trvomp.lower() in ['', '1', 'true']:
     for flag in ['-fopenmp', '-DTRV_USE_OMP', '-DTRV_USE_FFTWOMP']:
         if flag not in cflags:
@@ -67,16 +70,17 @@ if py_trvomp is not None and py_trvomp.lower() in ['', '1', 'true']:
 # -- Extensions ----------------------------------------------------------
 
 class BuildExt(build_ext):
-    """Modify :class:`Cython.Distutils.build_ext`.
+    """Modified :class:`Cython.Distutils.build_ext`.
 
     """
     def build_extensions(self):
         """Override the compiler and compilation options.
 
         """
-        # self.compiler.set_executable('compiler_cxx', compiler)
-        # self.compiler.set_executable('compiler_so', compiler)
-        # self.compiler.set_executable('linker_so', compiler)
+        # _compiler = os.environ.get('PY_CXX', compiler)
+        # self.compiler.set_executable('compiler_cxx', _compiler)
+        # self.compiler.set_executable('compiler_so', _compiler)
+        # self.compiler.set_executable('linker_so', _compiler)
 
         if '-Wstrict-prototypes' in self.compiler.compiler_so:
             self.compiler.compiler_so.remove('-Wstrict-prototypes')
@@ -84,7 +88,28 @@ class BuildExt(build_ext):
         super().build_extensions()
 
 
-def return_extension(module_name, extra_cpp_sources=None, **ext_kwargs):
+class BuildClib(build_clib):
+    """Modified :class:`setuptools.command.build_clib`.
+
+    """
+    def build_libraries(self, libraries):
+        """Override the compiler and compilation options.
+
+        """
+        # _compiler = os.environ.get('PY_CXX', compiler)
+        # self.compiler.set_executable('compiler_cxx', _compiler)
+        # self.compiler.set_executable('compiler_so', _compiler)
+        # self.compiler.set_executable('linker_so', _compiler)
+
+        if '-Wstrict-prototypes' in self.compiler.compiler_so:
+            self.compiler.compiler_so.remove('-Wstrict-prototypes')
+
+        super().build_libraries(libraries)
+
+
+def define_extension(module_name,
+                     auto_cpp_source=False, extra_cpp_sources=None,
+                     **ext_kwargs):
     """Return an extension from given source files and options.
 
     Parameters
@@ -92,10 +117,13 @@ def return_extension(module_name, extra_cpp_sources=None, **ext_kwargs):
     module_name : str
         Module name as in the built extension module
         ``<pkg_name>.<module_name>``. This sets the .pyx source file to
-        ``<pkg_dir>/<module_name>.pyx`` and the .cpp source file to
-        ``<pkg_dir>/src/modules/<module_name>.cpp``.
+        ``<pkg_dir>/<module_name>.pyx``.
+    auto_cpp_source : bool, optional
+        If `True` (default is `False`), add
+        ``<pkg_src_dir>/<module_name>.cpp`` to the list of source files
+        with any prefix underscores removed from ``<module_name>``.
     extra_cpp_sources : list of str, optional
-        Additional .cpp source files under ``<pkg_dir>/src/modules``
+        Additional .cpp source files under ``<pkg_src_dir>``
         (default is `None`).
     **ext_kwargs
         Options to pass to :class:`Cython.Distutils.Extension`.
@@ -119,12 +147,13 @@ def return_extension(module_name, extra_cpp_sources=None, **ext_kwargs):
     if ext_kwargs:
         ext_module_kwargs.update(ext_kwargs)
 
-    sources = [
-        os.path.join(pkg_dir, f"{module_name}.pyx"),
-        os.path.join(pkg_src, f"{module_name.lstrip('_')}.cpp"),
-    ]
+    sources = [os.path.join(pkg_dir, f"{module_name}.pyx"),]  # noqa: E231
+    if auto_cpp_source:
+        sources.append(
+            os.path.join(pkg_src_dir, f"{module_name}.cpp".lstrip('_'))
+        )
     for cpp_source in extra_cpp_sources:
-        sources.append(os.path.join(pkg_src, cpp_source))
+        sources.append(os.path.join(pkg_src_dir, cpp_source))
 
     return Extension(
         f'{pkg_name}.{module_name}',
@@ -132,7 +161,7 @@ def return_extension(module_name, extra_cpp_sources=None, **ext_kwargs):
     )
 
 
-# Set macros.
+# Set package macros, sources, include directories and libraries.
 pkg_macros = [
     ('TRV_EXTCALL', None),
     # ('TRV_USE_LEGACY_CODE', None),
@@ -140,70 +169,58 @@ pkg_macros = [
     # ('DBG_FLAG_NOAC', None),
 ]
 
+pkg_include_dir = os.path.join(pkg_dir, "include")
+
+pkg_src_dir = os.path.join(pkg_dir, "src/modules")
+
+pkg_library = (
+    'trv',
+    {
+        'sources': [
+            os.path.join(pkg_src_dir, cpp_source)
+            for cpp_source in os.listdir(pkg_src_dir)
+        ],
+        'include_dirs': [pkg_include_dir,],  # noqa: E231
+        'macros': pkg_macros,
+    }
+)
+
+# Set macros.
 npy_macros = [
     ('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION'),
 ]
 
 macros = pkg_macros + npy_macros
 
-# Set source, include and library paths.
-pkg_src = os.path.join(pkg_dir, "src/modules")
-
-pkg_include = os.path.join(pkg_dir, "include")
-
+# Set include directories.
 npy_include = numpy.get_include()
 
 ext_includes = [
-    incl for incl
-    in os.environ.get('PY_INCLUDES', "").replace("-I", "").split()
+    incl
+    for incl in os.environ.get('PY_INCLUDES', "").replace("-I", "").split()
     if pkg_dir not in incl
 ]
 
+includes = [pkg_include_dir, npy_include,] + ext_includes  # noqa: E231
+
+# Set libraries.
 ext_libs = ['gsl', 'gslcblas', 'm', 'fftw3', 'fftw3_omp',]  # noqa: E231
 
-includes = [pkg_include, npy_include,] + ext_includes  # noqa: E231
-libs = ext_libs
+libs = [] + ext_libs  # noqa: E231
 
 # Define extension modules.
-module_config = {
-    'parameters': {
-        'extra_cpp_sources': ["monitor.cpp",],  # noqa: E231
-    },
-    'dataobjs': {
-        'extra_cpp_sources': ["monitor.cpp", "parameters.cpp",],  # noqa: E231
-    },
-    '_particles': {
-        'extra_cpp_sources': ["monitor.cpp",],  # noqa: E231
-    },
-    '_twopt': {
-        'extra_cpp_sources': [
-            "monitor.cpp",
-            "maths.cpp",
-            "parameters.cpp",
-            "dataobjs.cpp",
-            "particles.cpp",
-            "field.cpp",
-        ],
-        'libraries': libs,
-    },
-    '_threept': {
-        'extra_cpp_sources': [
-            "monitor.cpp",
-            "maths.cpp",
-            "parameters.cpp",
-            "dataobjs.cpp",
-            "particles.cpp",
-            "field.cpp",
-            "twopt.cpp",
-        ],
-        'libraries': libs,
-    },
-    '_fftlog': {
-        'extra_cpp_sources': [
-            "monitor.cpp", "maths.cpp", "arrayops.cpp",
-        ],
-        'libraries': libs,
-    },
+ext_module_names = [
+    'parameters',
+    'dataobjs',
+    '_particles',
+    '_twopt',
+    '_threept',
+    '_fftlog',
+]
+
+ext_module_config = {
+    ext_mod_name: {'libraries': libs}
+    for ext_mod_name in ext_module_names
 }
 
 cython_directives = {
@@ -212,10 +229,10 @@ cython_directives = {
    'embedsignature': True,
 }
 
-cython_modules = [
-    return_extension(key, **val)
-    for key, val in module_config.items()
-]
+cython_ext_modules = cythonize(
+    [define_extension(name, **cfg) for name, cfg in ext_module_config.items()],
+    compiler_directives=cython_directives
+)
 
 
 # -- Set-up --------------------------------------------------------------
@@ -223,8 +240,10 @@ cython_modules = [
 if __name__ == '__main__':
     setup(
         # license=pkg_info.get('__license__'),
-        cmdclass={'build_ext': BuildExt},
-        ext_modules=cythonize(
-            cython_modules, compiler_directives=cython_directives
-        )
+        cmdclass={
+            'build_clib': BuildClib,
+            'build_ext': BuildExt,
+        },
+        ext_modules=cython_ext_modules,
+        libraries=[pkg_library,]  # noqa: E231
     )
