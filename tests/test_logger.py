6c4f5bb723aed93d28cc9e8000d4131fe8d88d5c
MikeSWang <wsb3513@gmail.com>
2023-03-09 01:30:29 +0000

    Fix parameter validation returns from C++. Add tests

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -4,10 +4,10 @@ import pytest
 
 from triumvirate.logger import setup_logger
 
-# _LOG_FORMAT = (
-#     r"\[\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\s\(\+\d+:\d{2}:\d{2}\)\s\w{4}\]"
-#     r"\s.*\(in C\+\+\)"
-# )
+_LOG_FORMAT = (
+    r"\[\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\s\(\+\d+:\d{2}:\d{2}\)\s\w{4}\]"
+    r"\s.*\(in C\+\+\)"
+)
 
 
 @pytest.fixture(scope='module')
@@ -15,10 +15,18 @@ def logger():
     return setup_logger()
 
 
-def test_setup_logger(logger, caplog):
+@pytest.mark.skip(
+    reason="Only succeeds when `pytest` is run with ``-s`` or ``--capture=no``"
+)
+def test_setup_logger_formatter(logger, capfd):
+    # Test logging formatter with C++ code state indication.
+    logger.info("Test log entry.", cpp_state=True)
+    assert re.match(_LOG_FORMAT, capfd.readouterr().out) is not None, \
+        "Misformatted logging."
 
-    # Test C++ code state indication.
+
+def test_setup_logger_adapter(logger, caplog):
+    # Test logger C++ code state indication.
     logger.info("Test log entry with C++ indication.", cpp_state=True)
-
     assert re.match(r'.*\(in C\+\+\)', caplog.text) is not None, \
         "No C++ indication shown by logger."
--- tests/test_parameters.py
+++ tests/test_parameters.py
@@ -1,145 +1,43 @@
 """Test :mod:`~triumvirate.parameters`.
 
 """
+import warnings
 from collections.abc import Collection, Sequence
+from copy import deepcopy
 
 import pytest
-import yaml
 
-from triumvirate.parameters import InvalidParameterError, NonParameterError
+from triumvirate.parameters import InvalidParameterError
 from triumvirate.parameters import ParameterSet
 from triumvirate.parameters import fetch_paramset_template
 from triumvirate.parameters import _TMPL_PARAM_DICT
 
 
-@pytest.fixture(scope='module')
-def template_parameters():
-    return fetch_paramset_template('dict')
+def make_template_parameters_valid(tmpl_params):
+    """Construct valid parameters from template parameters.
 
+    Parameters
+    ----------
+    tmpl_params : str or dict
+        Template parameters.
 
-@pytest.fixture(scope='module')
-def default_parameters():
-    return {
-        'alignment': 'centre',
-        'padscale': 'box',
-        'assignment': 'tsc',
-        'interlace': False,
-        'form': 'diag',
-        'norm_convention': 'particle',
-        'binning': 'lin',
-        'verbose': 20,
-    }
+    Returns
+    -------
+    str or dict
+        Valid parameters constructed from the template parameters.
 
+    Raises
+    ------
+    TypeError
+        If `template_parameters` type is not :class:`str`
+        or :class:`dict`.
 
-def test_fetch_paramset_template_text():
+    """
+    if isinstance(tmpl_params, str):
+        def replace_parameter(_text, str_original, str_new):
+            return _text.replace(str_original, str_new)
 
-    return
-
-
-@pytest.mark.parametrize(
-    "case, ret_defaults",
-    [
-        ('text', False),
-        ('dict', True),
-    ]
-)
-def test_fetch_paramset_template_dict(case, ret_defaults, default_parameters):
-
-    fetched = fetch_paramset_template(case, ret_defaults=ret_defaults)
-    if ret_defaults:
-        template, defaults = fetched
-    else:
-        template = fetched
-
-    # Check for 'text' case and exit early.
-    if case == 'text':
-        return
-    assert case == 'dict', f"Unrecognised argument value: {case}."
-
-    # Check `template` agrees with an independent, private template
-    # parameter dictionary.
-    assert template == _TMPL_PARAM_DICT, \
-        "Fetched template parameters do not match records."
-
-    # Check `defaults`` are pre-set default parameters.
-    assert defaults == default_parameters, (
-        "Fetched template default parameters do not match "
-        "pre-set default parameters."
-    )
-
-    # Check `template` minus `defaults` are unset.
-    non_defaults_keys = set(template.keys()) - set(defaults.keys())
-
-    for param_name in non_defaults_keys:
-        param_val = template[param_name]
-        if isinstance(param_val, Sequence):
-            assert all(
-                param_val_element is None for param_val_element in param_val
-            ), (
-                "Non-NoneType element found in "
-                "sequence-type non-default parameter value "
-                "in parameter template."
-            )
-        elif isinstance(param_val, Collection):
-            assert all(
-                param_val_val is None for param_val_val in param_val.values()
-            ), (
-                "Non-NoneType value found in "
-                "collection-type non-default parameter value "
-                "in parameter template."
-            )
-        else:
-            assert param_val is None, (
-                "Non-NoneType non-default parameter value "
-                "found in parameter template."
-            )
-
-
-@pytest.mark.parametrize(
-    'param_filepath, param_dict',
-    [
-        ("triumvirate/resources/params_template.yml", None),
-        (None, 'template_parameters'),
-    ]
-)
-def test_ParameterSet___init__(param_filepath, param_dict, request):
-
-    if param_dict is not None:
-        param_dict = request.getfixturevalue(param_dict)
-
-    # Check initialisation fails.
-    with pytest.raises(InvalidParameterError):
-        ParameterSet(
-            param_filepath=param_filepath, param_dict=param_dict
-        )
-
-    # Modify failed initialisation and check it succeeds.
-    if param_filepath is None and param_dict is not None:
-        for ax_name in ['x', 'y', 'z']:
-            param_dict['boxsize'][ax_name] = 1000.
-            param_dict['ngrid'][ax_name] = 64.
-
-        # NOTE: 'ell1' and 'ell2' are only mandatory for
-        # three-point statistic algorithms.
-        param_dict.update({
-            'catalogue_type': 'sim',
-            'statistic_type': 'bispec',
-            'degrees': {'ell1': 0, 'ell2': 0, 'ELL': 0},
-            'range': [0.005, 0.105],
-            'num_bins': 10,
-        })
-
-        paramset = ParameterSet(
-            param_filepath=param_filepath, param_dict=param_dict
-        )
-    elif param_filepath is not None and param_dict is None:
-
-        with open(param_filepath, 'r') as tmpl_parameter_file:
-            parameter_template = tmpl_parameter_file.read()
-
-        def replace_parameter(parameter_template, str_original, str_new):
-            return parameter_template.replace(str_original, str_new)
-
+        param_text = tmpl_params
         parameter_replacements = [
             (
                 "boxsize:\n  x:\n  y:\n  z:\n",
@@ -179,13 +77,213 @@ def test_ParameterSet___init__(param_filepath, param_dict, request):
             ),
         ]
 
-        for replacement in parameter_replacements:
-            replace_parameter(parameter_template, *replacement)
+        for _replacement in parameter_replacements:
+            param_text = replace_parameter(param_text, *_replacement)
 
-        yaml.
+        return param_text
 
-        paramset = ParameterSet(
+    if isinstance(tmpl_params, dict):
+        param_dict = deepcopy(tmpl_params)
+
+        for ax_name in ['x', 'y', 'z']:
+            param_dict['boxsize'][ax_name] = 1000.
+            param_dict['ngrid'][ax_name] = 64.
+
+        param_dict.update({
+            'catalogue_type': 'sim',
+            'statistic_type': 'bispec',
+            'degrees': {'ell1': 0, 'ell2': 0, 'ELL': 0},
+            'range': [0.005, 0.105],
+            'num_bins': 10,
+        })
+
+        return param_dict
+
+    raise TypeError("`template_parameters` is not str or dict.")
+
+
+# Returns the subset of default parameters from the template.
+@pytest.fixture(scope='module')
+def default_parameters():
+    return {
+        'alignment': 'centre',
+        'padscale': 'box',
+        'assignment': 'tsc',
+        'interlace': False,
+        'form': 'diag',
+        'norm_convention': 'particle',
+        'binning': 'lin',
+        'verbose': 20,
+    }
+
+
+@pytest.fixture(scope='module')
+def template_parameters_text():
+    return fetch_paramset_template('text')
+
+
+@pytest.fixture(scope='module')
+def template_parameters_dict():
+    return fetch_paramset_template('dict')
+
+
+@pytest.fixture(scope='module')
+def valid_paramset(template_parameters_dict):
+    return ParameterSet(
+        param_dict=make_template_parameters_valid(template_parameters_dict)
+    )
+
+
+@pytest.mark.parametrize(
+    "source, ret_defaults",
+    [
+        ('text', False),
+        ('dict', True),
+    ]
+)
+def test_fetch_paramset_template(source, ret_defaults, default_parameters):
+
+    fetched = fetch_paramset_template(source, ret_defaults=ret_defaults)
+    if ret_defaults:
+        template, defaults = fetched
+    else:
+        template = fetched
+
+    # Check for 'text' source and exit early; else assume 'dict' source.
+    if source == 'text':
+        return
+    if source != 'dict':
+        raise ValueError("Invalid `pytest.mark.parametrize` parameters.")
+
+    # Check `template` agrees with an independent, private template
+    # parameter dictionary.
+    assert template == _TMPL_PARAM_DICT, \
+        "Fetched template parameters do not match records."
+
+    # Check `defaults`` are pre-set default parameters.
+    assert defaults == default_parameters, (
+        "Fetched template default parameters do not match "
+        "pre-set default parameters."
+    )
+
+    # Check `template` minus `defaults` are unset.
+    non_defaults_keys = set(template.keys()) - set(defaults.keys())
+    for param_name in non_defaults_keys:
+        param_val = template[param_name]
+        if isinstance(param_val, Sequence):
+            assert all(
+                param_val_element is None for param_val_element in param_val
+            ), (
+                "Non-NoneType element found in "
+                "sequence-type non-default parameter value "
+                "in parameter template."
+            )
+        elif isinstance(param_val, Collection):
+            assert all(
+                param_val_val is None for param_val_val in param_val.values()
+            ), (
+                "Non-NoneType value found in "
+                "collection-type non-default parameter value "
+                "in parameter template."
+            )
+        else:
+            assert param_val is None, (
+                "Non-NoneType non-default parameter value "
+                "found in parameter template."
+            )
+
+
+@pytest.mark.parametrize(
+    'param_filepath, param_dict',
+    [
+        ("src/triumvirate/resources/params_template.yml", None),
+        (None, 'template_parameters_dict'),
+        (
+            "src/triumvirate/resources/params_template.yml",
+            'template_parameters_dict',
+        ),
+    ]
+)
+def test_ParameterSet___init__(param_filepath, param_dict, request, tmp_path):
+
+    # Convert parametrized parameters to fixtures.
+    if param_dict is not None:
+        param_dict = request.getfixturevalue(param_dict)
+
+    # Check initialisation fails (and exit early if needed).
+    if param_filepath is not None and param_dict is not None:
+        with pytest.raises(ValueError):
+            ParameterSet(
+                param_filepath=param_filepath, param_dict=param_dict
+            )
+        return
+
+    with pytest.raises(InvalidParameterError):
+        ParameterSet(
             param_filepath=param_filepath, param_dict=param_dict
         )
+
+    # Modify failed initialisation and check it succeeds.
+    if param_filepath is None and param_dict is not None:
+        param_dict = make_template_parameters_valid(param_dict)
+        ParameterSet(param_dict=param_dict)
+    elif param_filepath is not None and param_dict is None:
+        with open(param_filepath, 'r') as tmpl_parameter_file:
+            param_text = make_template_parameters_valid(
+                tmpl_parameter_file.read()
+            )
+
+        param_filepath = tmp_path/"params.yml"
+        with open(param_filepath, 'w') as param_file:
+            param_file.write(param_text)
+
+        ParameterSet(param_filepath=param_filepath)
     else:
-        raise ValueError("Invalid pytest.mark.parametrize parameters.")
+        raise ValueError("Invalid `pytest.mark.parametrize` parameters.")
+
+
+@pytest.mark.parametrize(
+    'template_parameter_source',
+    [
+        'template_parameters_text',
+        'template_parameters_dict',
+    ]
+)
+def test_ParameterSet___str__(template_parameter_source, request, tmp_path):
+
+    valid_parameter_source = make_template_parameters_valid(
+        request.getfixturevalue(template_parameter_source)
+    )
+
+    if isinstance(valid_parameter_source, str):
+        param_filename = "params.yml"
+        with open(tmp_path/param_filename, 'w') as param_file:
+            param_file.write(valid_parameter_source)
+
+        paramset = ParameterSet(param_filepath=tmp_path/param_filename)
+        assert param_filename in str(paramset)
+        assert 'original=True' in str(paramset)
+        return
+
+    if isinstance(valid_parameter_source, dict):
+        paramset = ParameterSet(param_dict=valid_parameter_source)
+        assert 'dict' in str(paramset)
+        assert 'original=True' in str(paramset)
+        return
+
+    raise ValueError(
+        "Invalid `valid_parameter_source` fixture parametrisation."
+    )
+
+
+# Use default parameters to test the valid parameter set.
+def test_ParameterSet___getitem__(valid_paramset, default_parameters):
+    for key, val in default_parameters.items():
+        # This is three tests in one: TypeError, KeyError and value comparison.
+        if valid_paramset[key] != val:
+            warnings.warn(
+                "`valid_paramset` fixture does not match default parameters "
+                "for key '{}': {} != {}."
+                .format(key, valid_paramset[key], val),
+                category=RuntimeWarning
+            )
