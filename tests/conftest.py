83a2e055798d3baff7ebd8c1466dc06c25e520f5
Mike S Wang <32841762+MikeSWang@users.noreply.github.com>
2023-03-14 17:34:37 +0000

    Update parameter set in Python

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,8 +4,10 @@
 import inspect
 from pathlib import Path
 
+import numpy as np
 import pytest
 
+from triumvirate.catalogue import ParticleCatalogue
 from triumvirate.logger import setup_logger
 from triumvirate.parameters import ParameterSet
 
@@ -68,6 +70,11 @@ def test_ctlg_dir(test_input_dir):
 
 
 @pytest.fixture(scope='session')
+def test_stats_dir(test_input_dir):
+    return test_input_dir/"statistics"
+
+
+@pytest.fixture(scope='session')
 def logger():
     return setup_logger()
 
@@ -75,3 +82,42 @@ def logger():
 @pytest.fixture(scope='session')
 def valid_paramset(test_param_dir):
     return ParameterSet(param_filepath=test_param_dir/"valid_params.yml")
+
+
+@pytest.fixture(scope='session')
+def test_catalogue_properties():
+    return {
+        'separation': 100.,
+        'boxsize': 1.e3,
+        'nparticle': 3,
+        'contrast': 1.e4,
+    }
+
+
+@pytest.fixture(scope='session')
+def test_data_catalogue(test_catalogue_properties):
+
+    R = test_catalogue_properties['separation']
+    L = test_catalogue_properties['boxsize']
+    N = test_catalogue_properties['nparticle']
+
+    x = [-R/2., 0., R/2.]
+    y = [-R/2./np.sqrt(3), R/np.sqrt(3), -R/2./np.sqrt(3)]
+    z = [0., 0., 0.]
+    nz = N / L**3
+
+    return ParticleCatalogue(x, y, z, nz=nz)
+
+
+def test_rand_catalogue(test_catalogue_properties):
+
+    alpha = test_catalogue_properties['contrast']
+    L = test_catalogue_properties['boxsize']
+    N = test_catalogue_properties['nparticle']
+
+    generator = np.random.default_rng(seed=42)
+
+    x, y, z = generator.uniform(-L/2., L/2., size=(3, int(alpha*N)))
+    nz = N / L**3
+
+    return ParticleCatalogue(x, y, z, nz=nz)
