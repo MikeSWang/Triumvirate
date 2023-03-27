5f27869766cf4d072594b3221cc62742eddf4004
MikeSWang <wsb3513@gmail.com>
2023-03-27 16:18:45 +0100

    Update dist workflow

--- tools/pkg/autorel_testpypi.sh
+++ tools/pkg/autorel_testpypi.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 #
-# @file autotest_vers.sh
+# @file autorel_testpypi.sh
 # @author Mike S Wang
 # @brief Upload release to test index repositories.
 #
 
 rm -rf dist/
 python -m pip install --upgrade wheel build auditwheel twine
-python -m build
+python -m build --sdist --wheel --outdir dist/ .
 auditwheel -v repair dist/Triumvirate-*.whl
 python -m twine upload --verbose --repository testpypi dist/*
