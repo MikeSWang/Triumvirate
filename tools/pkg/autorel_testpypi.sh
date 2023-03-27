56fb13157ae59688708190b3bd13d6dc02e8bd8c
Mike S Wang <32841762+MikeSWang@users.noreply.github.com>
2023-03-27 23:43:23 +0100

    Update build config and dist workflows

--- tools/pkg/autorel_testpypi.sh
+++ tools/pkg/autorel_testpypi.sh
@@ -5,8 +5,17 @@
 # @brief Upload release to test index repositories.
 #
 
+# Clean distribution directory.
 rm -rf dist/
+
+# Install distribution tools.
 python -m pip install --upgrade build auditwheel twine
+
+# Build both 'sdist' and 'bdist'.
 python -m build --sdist --wheel --outdir dist/ .
+
+# Audit built wheels.
 auditwheel -v repair dist/Triumvirate-*.whl
-python -m twine upload --verbose --repository testpypi dist/*
+
+# # Upload 'sdist' only for now.
+# python -m twine upload --verbose --repository testpypi dist/*.tar.gz
