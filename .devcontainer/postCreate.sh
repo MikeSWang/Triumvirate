e5d976dcd4e4b3d177d2d1f0279013bcddc65c50
Mike S Wang <32841762+MikeSWang@users.noreply.github.com>
2023-11-10 16:14:53 +0000

    Refactor notice

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -2,9 +2,11 @@
 conda init bash
 conda init zsh
 
-# # Imitate `postStartCommand` using configuration files for each shell.
+# Imitate `postStartCommand` using configuration files for each shell.
 # cat .devcontainer/postStart.sh >> $HOME/.bashrc
 # cat .devcontainer/postStart.sh >> $HOME/.zshrc
+echo "alias welcome-notice='cat /usr/local/etc/vscode-dev-containers/first-run-notice.txt'" >> $HOME/.bashrc
+echo "alias welcome-notice='cat /usr/local/etc/vscode-dev-containers/first-run-notice.txt'" >> $HOME/.zshrc
 
 # Fetch Git tags.
 git fetch --tags
