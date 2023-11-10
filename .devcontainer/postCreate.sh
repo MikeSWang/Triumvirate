90ab1aa75afd54368d6fb318327eafb94ab56383
Mike S Wang <32841762+MikeSWang@users.noreply.github.com>
2023-11-10 15:58:19 +0000

    Refactor dev container post-start

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -2,9 +2,9 @@
 conda init bash
 conda init zsh
 
-# Imitate `postStartCommand` using configuration files for each shell.
-cat .devcontainer/postStart.sh >> $HOME/.bashrc
-cat .devcontainer/postStart.sh >> $HOME/.zshrc
+# # Imitate `postStartCommand` using configuration files for each shell.
+# cat .devcontainer/postStart.sh >> $HOME/.bashrc
+# cat .devcontainer/postStart.sh >> $HOME/.zshrc
 
 # Fetch Git tags.
 git fetch --tags
