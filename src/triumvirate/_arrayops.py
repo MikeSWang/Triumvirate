3151d6af191d87a7927474abcb90e68080a62354
Mike S Wang <32841762+MikeSWang@users.noreply.github.com>
2024-03-12 00:40:08 +0000

    Refactor 3-pt stat array reshaping method

--- src/triumvirate/_arrayops.py
+++ src/triumvirate/_arrayops.py
@@ -716,3 +716,66 @@ def extrap2d_loglin_oscil(a, n_ext):
     a_out = _extrap2d_row_loglin_oscil(a_.transpose(), nrow_ext).transpose()
 
     return a_out
+
+
+# ========================================================================
+# Reshaping
+# ========================================================================
+
+def reshape_threept_datatab(paired_coords, flat_multipole):
+    """Reshape flattened full-form three-point clustering measurements
+    into 2-d arrays.
+
+    Parameters
+    ----------
+    paired_coords : 2-d array of float
+        Paired coordinate points, e.g. rows of :math:`(k_1, k_2)` values.
+    flat_multipole : 1-d array of float
+        Flattened multipole values corresponding to the
+        paired coordinates, assuming the two coordinate variables
+        are sampled at the same points.
+
+    Returns
+    -------
+    common_coords : 1-d array of float
+        Common coordinate points as a vector.
+    multipole : 2-d array of float
+        Multipole values as a symmetric matrix.
+
+    Raises
+    ------
+    ValueError
+        If `paired_coords` is not a 2-d array.
+    ValueError
+        If `paired_coords` and `flat_multipole` have different lengths.
+    ValueError
+        If `paired_coords` and `flat_multipole` do not correspond to an
+        upper triangular matrix.
+
+    """
+    # Check dimensions and reshape.
+    if np.array(paired_coords).ndim != 2:
+        raise ValueError("Paired coordinate points must be a 2-d array.")
+    if (flat_len := len(paired_coords)) != len(flat_multipole):
+        raise ValueError(
+            "Paired coordinate points and flattened multipole "
+            "array must have the same length."
+        )
+
+    nbins = (np.sqrt(8*flat_len + 1) - 1) / 2.
+    if not np.isclose(nbins, int(nbins)):
+        raise ValueError(
+            "Input arrays do not correspond to an upper triangular matrix."
+        )
+    nbins = int(nbins)
+
+    # Reshape the flattened multipole array into a symmetric matrix.
+    common_coords = paired_coords[:nbins, 1]
+
+    triu_indices = np.triu_indices(nbins)
+
+    multipole = np.zeros((nbins, nbins))
+    multipole[triu_indices] = flat_multipole
+    multipole.T[triu_indices] = flat_multipole
+
+    return common_coords, multipole
