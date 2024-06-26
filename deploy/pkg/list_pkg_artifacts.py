b1c0b56183530470756e9ed7445cd330030b0a41
Mike S Wang <32841762+MikeSWang@users.noreply.github.com>
2024-06-26 17:31:37 +0100

    Add packaging scripts

---
+++ deploy/pkg/list_pkg_artifacts.py
@@ -0,0 +1,206 @@
+"""List package artifacts for a release tag.
+
+"""
+import json
+import os
+import re
+import subprocess as subp
+from argparse import ArgumentParser
+from functools import reduce
+from operator import getitem
+from pprint import pprint
+
+
+def configure():
+    """Configure command-line arguments.
+
+    Returns
+    -------
+    args : argparse.Namespace
+        Parsed command-line arguments.
+
+    """
+    parser = ArgumentParser(
+        description='List package artifact IDs for a release tag.'
+    )
+
+    parser.add_argument('tag', help='release tag')
+    parser.add_argument(
+        '--github-owner',
+        default=os.environ.get('GITHUB_OWNER', 'MikeSWang'),
+        help="GitHub owner"
+    )
+    parser.add_argument(
+        '--github-repo',
+        default=os.environ.get('GITHUB_REPO', 'Triumvirate'),
+        help="GitHub repository"
+    )
+    parser.add_argument(
+        '--filter', action='append', nargs=2,
+        metavar=('KEYCHAIN', 'VALUE_PATTERN'),
+        default=[],
+        help="additive filter keychain and value pattern, e.g. "
+             "``--filter 'name' 'pypi_dist_*' "
+             "--filter 'workflow_run.head_branch' 'main'`` "
+             "filters artifacts with name starting with 'pypi_dist_' "
+             "and workflow run head branch 'main'"
+    )
+    parser.add_argument(
+        '--id-only', action='store_true',
+        help="list only artifact IDs"
+    )
+
+    args = parser.parse_args()
+
+    return args
+
+
+def get_artifact_data(config):
+    """Get package artifact data for a release tag.
+
+    Parameters
+    ----------
+    config : argparse.Namespace
+        Configured parameters.
+
+    Returns
+    -------
+    artifact_data : dict
+        Package artifact data.
+
+    """
+    github_header_accept = os.environ.get(
+        'GITHUB_HEADER_ACCEPT',
+        "Accept: application/vnd.github+json"
+    )
+
+    github_header_auth = os.environ.get('GITHUB_HEADER_AUTH', None)
+    if github_header_auth is None:
+        github_header_auth_prefix = "Authorization: token "
+        github_token_artifacts = ''
+        try:
+            github_token_artifacts = os.environ['GITHUB_TOKEN_ARTIFACTS']
+        except KeyError:
+            try:
+                github_token_artifacts = os.environ['GITHUB_TOKEN']
+            except KeyError:
+                raise RuntimeError(
+                    "Neither of the following environment variable is set: "
+                    "GITHUB_TOKEN, GITHUB_TOKEN_ARTIFACTS"
+                )
+        github_header_auth = github_header_auth_prefix + github_token_artifacts
+
+    github_url_actions = os.environ.get(
+        'GITHUB_URL_ACTIONS',
+        "https://api.github.com/repos/"
+        f"{config.github_owner}/{config.github_repo}/"
+        "actions/artifacts"
+    )
+
+    cmd_fields = [
+        'curl', '-s', '-L',
+        '-H', github_header_accept,
+        '-H', github_header_auth,
+        github_url_actions
+    ]
+
+    std_result = subp.run(cmd_fields, stdout=subp.PIPE)
+    artifact_data = json.loads(std_result.stdout.decode('utf-8'))
+
+    return artifact_data
+
+
+def filter_pkg_artifacts(artifact_data, config):
+    """Filter package artifacts.
+
+    Parameters
+    ----------
+    artifact_data : dict
+        Package artifact data.
+    config : argparse.Namespace
+        Configured parameters.
+
+    Returns
+    -------
+    artifact_list : list of dict
+        Filtered package artifact data.
+
+    """
+    filters = config.filter or []
+
+    artifact_list = []
+    for artifact in artifact_data['artifacts']:
+        for (keychain, pattern) in filters:
+            excluded = not bool(
+                re.match(
+                    pattern,
+                    reduce(getitem, keychain.split('.'), artifact),
+                )
+            )
+            if excluded:
+                break
+        else:
+            artifact_list.append(artifact)
+
+    return artifact_list
+
+
+def get_latest_artifacts(artifacts_list):
+    """Get only the latest artifacts when multiple IDs have the same name.
+
+    Parameters
+    ----------
+    artifacts_list : list of dict
+        Package artifact list.
+
+    Returns
+    -------
+    artifacts_latest_list : list of dict
+        Latest package artifact list.
+
+    """
+    unique_names = set(artifact['name'] for artifact in artifacts_list)
+
+    latest_ids = {}
+    for name in unique_names:
+        latest_ids[name] = max(
+            artifact['id']
+            for artifact in artifacts_list
+            if artifact['name'] == name
+        )
+
+    artifacts_latest_list = [
+        artifact for artifact in artifacts_list
+        if artifact['id'] in latest_ids.values()
+    ]
+
+    return artifacts_latest_list
+
+
+def list_pkg_artifacts(artifacts_list, id_only=False):
+    """List package artifacts.
+
+    Parameters
+    ----------
+    artifacts_list : list of dict
+        Package artifact list.
+    id_only : bool, optional
+        If True, only print artifact IDs.
+
+    """
+    if id_only:
+        for artifact in artifacts_list:
+            print(artifact['id'])
+    else:
+        for artifact in artifacts_list:
+            pprint(artifact)
+
+
+if __name__ == '__main__':
+
+    cfg = configure()
+    artifact_data = get_artifact_data(cfg)
+    artifact_list = get_latest_artifacts(
+        filter_pkg_artifacts(artifact_data, cfg)
+    )
+    list_pkg_artifacts(artifact_list, id_only=cfg.id_only)
---
+++ deploy/pkg/prepare_pkg_artifacts.sh
@@ -0,0 +1,61 @@
+#! /bin/bash
+# @file prepare_pkg_artifacts.sh
+# @author Mike S Wang
+# @brief Prepare package artifacts from GitHub Actions.
+# @arg release_tag, the release tag to download artifacts for.
+#
+
+release_tag=$1
+
+tmp_dir=$(mktemp -d)
+
+# GitHub attributes
+GITHUB_OWNER=MikeSWang
+GITHUB_REPO=Triumvirate
+GITHUB_TOKEN_ARTIFACTS="$(sed '2q;d' ~/.github_auth)"
+GITHUB_URL_ACTIONS="https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPO}/actions/artifacts"
+
+GITHUB_HEADER_ACCEPT="Accept: application/vnd.github+json"
+GITHUB_HEADER_AUTH="Authorization: Bearer ${GITHUB_TOKEN_ARTIFACTS}"
+
+# Query artifact IDs.
+# e.g.
+# artifact_names="pypi_dist_macos-13_v0.4.7.post1 conda_dist_macos-13_v0.4.7.post1"
+artifact_ids=()
+artifact_names="pypi_dist.*${release_tag} conda_dist.*${release_tag}"
+for artifact_name in ${artifact_names};
+do
+    id=$(python deploy/pkg/list_pkg_artifacts.py ${release_tag} --filter name "$artifact_name" --id-only)
+    artifact_ids+=(${id})
+done
+
+# Download and unzip artifacts.
+dist_dir=${tmp_dir}/dist
+mkdir -p ${dist_dir}
+
+echo "Downloading artifacts to temporary directory for extraction: ${tmp_dir}"
+for artifact_id in "${artifact_ids[@]}";
+do
+    archive_file=${dist_dir}/${artifact_id}.zip
+    curl -L -H "${GITHUB_HEADER_ACCEPT}" -H "${GITHUB_HEADER_AUTH}" \
+        -o ${archive_file} "${GITHUB_URL_ACTIONS}/${artifact_id}/zip"
+    unzip ${archive_file} -d ${dist_dir} && rm ${archive_file}
+done
+
+# Organise artifacts.
+release_dir=${tmp_dir}/releases
+
+mkdir -p ${release_dir}/github-release
+find ${dist_dir} -type f -name '*.conda' -exec mv {} ${release_dir}/github-release \;
+
+mkdir -p ${release_dir}/pypi-release
+find ${dist_dir} -type f \( -name '*.tar.gz' -or -name '*.whl' \) -exec mv {} ${release_dir}/pypi-release \;
+
+mkdir -p ${release_dir}/conda-release
+find ${dist_dir} -type f -name '*.tar.bz2' -exec mv {} ${release_dir}/conda-release \;
+
+find ${release_dir} -mindepth 1 -type d ! -name '*-release' -exec rm -rf {} +
+
+mv ${release_dir} ~/Downloads/
+
+rm -rf ${tmp_dir}
