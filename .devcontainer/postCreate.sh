f3d727c7523e80c2fa717c42823de315ea207f6c
Mike S Wang <32841762+MikeSWang@users.noreply.github.com>
2024-06-29 01:45:13 +0100

    MAINT: Refactor path variables

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -11,8 +11,9 @@ conda init zsh
 # Imitate `postStartCommand` using configuration files for each shell.
 # cat .devcontainer/postStart.sh >> "$HOME"/.bashrc
 # cat .devcontainer/postStart.sh >> "$HOME"/.zshrc
-echo "alias welcome-notice='cat /usr/local/etc/vscode-dev-containers/first-run-notice.txt'" >> "$HOME"/.bashrc
-echo "alias welcome-notice='cat /usr/local/etc/vscode-dev-containers/first-run-notice.txt'" >> "$HOME"/.zshrc
+NOTICE_FILE="/usr/local/etc/vscode-dev-containers/first-run-notice.txt"
+echo "alias welcome-notice='cat ${NOTICE_FILE}'" >> "$HOME"/.bashrc
+echo "alias welcome-notice='cat ${NOTICE_FILE}'" >> "$HOME"/.zshrc
 
 # Fetch Git tags.
 git fetch --tags
