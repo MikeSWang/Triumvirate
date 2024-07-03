e2a4151e3352c0ebe3e66c9cb8f92e3f5fe473fa
Mike S Wang <32841762+MikeSWang@users.noreply.github.com>
2024-07-04 00:02:42 +0100

    MAINT: Update pre-release operations in dry-run mode

--- deploy/pkg/prerelease_ops.sh
+++ deploy/pkg/prerelease_ops.sh
@@ -2,8 +2,11 @@
 #
 # @file prerelease_ops.sh
 # @author Mike S Wang
-# @brief Pre-release operations.
+# @brief Pre-release operations in dry-run mode.
 #
 
-semantic-release --noop version --print 2> /dev/null
-semantic-release changelog 2> /dev/null
+THIS_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+export PYTHONPATH="${PYTHONPATH}:${THIS_DIR}"
+
+semantic-release -v changelog 2> /dev/null
+semantic-release -v --noop version --no-push # 2> /dev/null
