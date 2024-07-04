9499962fc9cf206c975317222b7da1547f37f2e3
Mike S Wang <32841762+MikeSWang@users.noreply.github.com>
2024-07-04 11:24:31 +0100

    MAINT: Fix typo

--- deploy/pkg/prerelease_ops.sh
+++ deploy/pkg/prerelease_ops.sh
@@ -9,4 +9,4 @@ THIS_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 export PYTHONPATH="${PYTHONPATH}:${THIS_DIR}"
 
 semantic-release -v changelog 2> /dev/null
-semantic-release -v --noop version --no-push # 2> /dev/null
+semantic-release -v --noop version # 2> /dev/null
