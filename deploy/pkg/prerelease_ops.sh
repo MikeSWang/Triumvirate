fce377cc8bd2fafd9141bb352b5d0305f05524eb
Mike S Wang <32841762+MikeSWang@users.noreply.github.com>
2024-09-10 13:41:33 +0100

    MAINT(cfg): Refactor configuration files

--- deploy/pkg/prerelease_ops.sh
+++ deploy/pkg/prerelease_ops.sh
@@ -6,7 +6,8 @@
 #
 
 THIS_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+CONFIG_FILE="${THIS_DIR}/../../.pyproject.toml"
 export PYTHONPATH="${PYTHONPATH}:${THIS_DIR}"
 
-semantic-release -v changelog 2> /dev/null
-semantic-release -v --noop version # 2> /dev/null
+semantic-release -v --config ${CONFIG_FILE} changelog 2> /dev/null
+semantic-release -v --config ${CONFIG_FILE} --noop version # 2> /dev/null
